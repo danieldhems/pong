7e30faaf2e8256962205d6f42205e5dc35d5a56f
Dan Hems <danhems87@gmail.com>
2015-01-03 18:26:42 +0000

    Ball collision and deflection logic in playable state. Scores imcremented and displayed for both players.

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ var Pong = function(){
 
 	var self = this;
 
-	this.direction = "forward";
+	this.directionX = "right";
+	this.directionY = undefined;
 
 	this.controls = {
+		start: 32, // spacebar
 		player1: {
 			"up": 81, // "Q"
 			"down": 65 // "A"
@@ -15,56 +17,120 @@ var Pong = function(){
 		}
 	};
 
+	this.points = {
+		player1: 0,
+		player2: 0
+	};
+
 	this.playerPoint; // Player to be awarded next point on miss (use in conjunction with ball direction)
 
 	this.player1Paddle;
 	this.player2Paddle;
 
+	this.config = {
+		paddleHeight: 50
+	};
+
 	this.keyDown = false;
+
+	this.tween - function(){
+		var steps = [10,10,10,10,9,8,7,6,5],
+			frames = 20;
+
+		this.startAnimation = function(){}
+	}
+
+	this.getUnitlessNum = function(value){
+		return parseInt(value.replace(/[a-zA-Z]/g, ''));
+	}
+
+	this.ballYVector = 3;
+
+	this.player1KeyIsDown = false,
+	this.player1KeyIsUp = true;
 
 	this.bindControls = function(){
 
+		var paddleMove = undefined;
+
 		window.addEventListener("keyup", function(e){
+
+			self.player1KeyIsDown = false;
+
 			if(
 				e.keyCode === self.controls.player1.up
 				|| e.keyCode === self.controls.player1.down
 				|| e.keyCode === self.controls.player2.up
 				|| e.keyCode === self.controls.player2.down
-			) self.keyDown = false;
+			){
+				if(paddleMove) clearInterval(paddleMove);
+			}
 		});
 
 		window.addEventListener("keydown", function(e){
 
+			//self.player1KeyIsDown = true;
+			
 			switch(e.keyCode){
+				case self.controls.start:
+					self.tick();
+				break;
 				case self.controls.player1.up:
-				self.player1Paddle.style.top = parseInt(self.player1Paddle.style.top) - 36 + "px";
+
+					var paddle = self.player1Paddle;
+
+					var paddleTop = paddle.offsetTop,
+						paddleBottom =  paddleTop + self.config.paddleHeight;
+
+					if( paddleTop - 1 > 0){
+						self.player1Paddle.style.top = paddleTop - 25 + "px";
+					}
+
 				break;
 
 				case self.controls.player1.down:
-				self.player1Paddle.style.top = parseInt(self.player1Paddle.style.top) + 36 + "px";
+					var paddle = self.player1Paddle,
+						paddleTop = paddle.offsetTop,
+						paddleBottom = paddle.offsetTop + self.config.paddleHeight;
+
+					if( paddleBottom + 1 < self.getBoundingBox(window).y){
+						self.player1Paddle.style.top = paddleTop + 25 + "px";
+					}
 				break;
 
 				case self.controls.player2.up:
-				self.player2Paddle.style.top = parseInt(self.player2Paddle.style.top) - 36 + "px";
+					var paddle = self.player2Paddle,
+						paddleTop = paddle.offsetTop,
+						paddleBottom = paddle.offsetTop + self.config.paddleHeight;
+
+					if( paddleBottom + 1 < self.getBoundingBox(window).y){
+						self.player2Paddle.style.top = paddleTop - 25 + "px";
+					}
 				break;
 				
 				case self.controls.player2.down:
-				self.player2Paddle.style.top = parseInt(self.player2Paddle.style.top) + 36 + "px";
+					var paddle = self.player2Paddle,
+						paddleTop = paddle.offsetTop,
+						paddleBottom = paddle.offsetTop + self.config.paddleHeight;
+
+					if( paddleBottom + 1 < self.getBoundingBox(window).y){
+						self.player2Paddle.style.top = paddleTop + 25 + "px";
+					}
 				break;	
 			}
 		});
 
 	}	
 
+
 	this.makeBall = function (width, height){
 		this.ball = document.createElement("div");
 		this.ball.style.width = width + "px";
 		this.ball.style.height = height + "px";
 		this.ball.style.position = "absolute";
-		this.ball.style.backgroundColor = "grey";
-		this.ball.style.borderRadius = "50px";
-		this.ball.style.top = "200px";
-		this.ball.style.left = 0;
+		this.ball.style.backgroundColor = "#fff";
+		this.ball.style.top = self.getBoundingBox(window).y / 2 + "px",
+		this.ball.style.left = (self.getBoundingBox(window).x / 2) - (width / 2) + "px";
 
 		document.body.appendChild(this.ball);
 	}
@@ -76,69 +142,150 @@ var Pong = function(){
 		this.player1Paddle.style.width = width + "px";
 		this.player1Paddle.style.height = size + "px";
 		this.player1Paddle.style.position = "absolute";
-		this.player1Paddle.style.backgroundColor = "grey";
-		this.player1Paddle.style.top = "200px";
+		this.player1Paddle.style.backgroundColor = "#fff";
+		this.player1Paddle.style.top = self.getBoundingBox(window).y / 2 - size / 2 + "px";
 		this.player1Paddle.style.left = "20px";
 		document.body.appendChild(this.player1Paddle);
 
 		this.player2Paddle.style.width = width + "px";
 		this.player2Paddle.style.height = size + "px";
 		this.player2Paddle.style.position = "absolute";
-		this.player2Paddle.style.backgroundColor = "grey";
-		this.player2Paddle.style.top = "200px";
+		this.player2Paddle.style.backgroundColor = "#fff";
+		this.player2Paddle.style.top = self.getBoundingBox(window).y / 2 - size / 2 + "px";
 		this.player2Paddle.style.right = "20px";
 		document.body.appendChild(this.player2Paddle);
 
 	}
 
-	this.startBall = function (direction, speed){
-		var journey = setInterval( function(){
+	this.addPoint = function(player){
+		self.points[player] += 1;
+		document.querySelector("#"+player).innerHTML = self.points[player];
+	}
 
-			var currentX = !self.ball.style.left ? 0 : parseInt(self.ball.style.left);
+	this.tick = function (speed){
+		var tick = setInterval( function(){
+
+			var currentX = !self.ball.style.left ? 0 : parseInt(self.ball.style.left),
+				currentY = !self.ball.style.top ? 0 : parseInt(self.ball.style.top),
+
+				ball = self.ball,
+				ballTop = ball.offsetTop,
+				ballBottom = ball.offsetTop + parseInt(ball.style.height),
+
+				windowTop = 0,
+				windowLeft = 0,
+				windowBottom = self.getBoundingBox(window).y,
+				windowRight = self.getBoundingBox(window).x;
 
 			/**
 			*	Ball movement logic
 			*/
 
-			/*
-			if( self.getBoundingBox(window).x < currentX + parseInt(self.ball.style.width)){
-				self.direction = "backward";
-			} else if(0 == currentX){
-				self.direction = "forward";
-			}
-			*/
-
-			if(self.direction == "forward"){
+			// horizontal movement
+			if(self.directionX == "right"){
 				self.ball.style.left = currentX + 3 + "px";
-			} else if(self.direction == "backward") {
+				//self.ball.style.top = currentY - (25/90) + "px";
+			} else if(self.directionX == "left") {
 				self.ball.style.left = currentX - 3 + "px";
 			}
 
-			if(self.direction == "forward"){
-				if(self.paddleCollision(self.player2Paddle)) self.direction = "backward";
-			} else if (self.direction == "backward"){
-				if(self.paddleCollision(self.player1Paddle)) self.direction = "forward";
+			// vertical movement
+			if(self.directionY == "up"){
+				self.ball.style.top = currentY - self.ballYVector + "px";
+			} else if(self.directionY == "down") {
+				self.ball.style.top = currentY + self.ballYVector + "px";
 			}
 
-			self.direction == "forward" ? self.paddleCollision( self.player2Paddle ) : self.paddleCollision( self.player1Paddle);
+			// paddle collision
+			if(self.isPaddleCollision(self.player2Paddle)) {
+				var trajectoryData = self.returnTrajectory(self.player2Paddle),
+					vector = trajectoryData.vector,
+					directionY = trajectoryData.directionY;
 
-		}, speed);
-	}.bind(this);
+				self.ballYVector = vector;
+				self.directionY = directionY;
 
-	this.paddleCollision = function(paddle){
+				self.directionX = "left";
+			}
+
+			if(self.isPaddleCollision(self.player1Paddle)){
+				var trajectoryData = self.returnTrajectory(self.player1Paddle),
+					vector = trajectoryData.vector,
+					directionY = trajectoryData.directionY;
+
+				self.ballYVector = vector;
+				self.directionY = directionY;
+
+				self.directionX = "right";
+			}
+		
+			// roof and floor collision
+			if(ballBottom >= windowBottom) self.directionY = "up";
+			if(ballTop <= windowTop) self.directionY = "down";
+
+			// point scoring when ball missed by paddles
+			if(currentX > windowRight) {
+				self.addPoint("player1");
+				resetTick();
+			}
+			if(currentX < windowLeft){
+				self.addPoint("player2");
+				resetTick();
+			}
+		},1);
+
+		function resetTick(){
+			clearInterval(tick);
+			self.ball.style.left = self.getBoundingBox(window).x / 2 + "px";
+			self.ball.style.top = self.getBoundingBox(window).y / 2 + "px";
+			self.directionY = undefined;
+		}
+	};
+
+
+	// need to detect which Y position ball collides to determine at which angle to return ball
+
+	this.returnTrajectory = function(paddle){
+		var paddleHeight = parseInt(paddle.style.height),
+			paddleTop = paddle.offsetTop,
+
+			windowHeight = self.getBoundingBox(window).y,
+
+			ballTop = self.ball.offsetTop,
+			ballHeight = parseInt(self.ball.style.height),
+			ballCenter = ballHeight / 2;
+			
+		var collisionPoint = ballTop - paddleTop;
+
+		var vector = (function(){
+			return collisionPoint <= 25 || collisionPoint >= 75 ? 3
+				:  collisionPoint > 25 && collisionPoint <= 45 || collisionPoint < 75 && collisionPoint >= 60 ? 2
+				:  1;
+		})();
+
+		return {
+			"vector": vector,
+			"directionY": collisionPoint <= 49 ? "up" : collisionPoint >= 51 ? "down" : undefined
+		}
+	}
+
+	this.isPaddleCollision = function(paddle){
 		var paddleTop = paddle.offsetTop,
 			paddleBottom = paddleTop + parseInt(paddle.style.height),
 			paddleFace = ( function(){
 				return paddle === self.player1Paddle ? self.player1Paddle.offsetLeft + parseInt(self.player1Paddle.style.width) : self.player2Paddle.offsetLeft;
-			}());
+			}()),
 
-		if(paddle ===  self.player1Paddle){
+			ballTop = self.ball.offsetTop,
+			ballBottom = self.ball.offsetTop + parseInt(self.ball.style.height);
+
+		if(paddle === self.player1Paddle){
 			if(
 				parseInt(self.ball.style.left) <= paddleFace
 				&&
-				self.ball.offsetTop >= paddleTop
+				ballBottom >= paddleTop
 				&&
-				self.ball.offsetTop <= paddleBottom
+				ballTop <= paddleBottom
 			){
 				return true;
 			}
@@ -147,9 +294,9 @@ var Pong = function(){
 			if(
 				parseInt(self.ball.style.left) + parseInt(self.ball.style.width) >= paddleFace
 				&&
-				self.ball.offsetTop >= paddleTop
+				ballBottom >= paddleTop
 				&&
-				self.ball.offsetTop <= paddleBottom
+				ballTop <= paddleBottom
 			){
 				return true;
 			}
@@ -166,10 +313,8 @@ var Pong = function(){
 
 	this.init = function (){
 		this.makeBall(20, 20);
-		this.makePaddles(5, 50);
-		this.startBall(1);
+		this.makePaddles(5,100);
 		this.bindControls();
-		this.paddleCollision(self.player1Paddle)
 	}
 
 	this.init();
@@ -179,5 +324,7 @@ var Pong = function(){
 window.onload = function(){
 
 	var pong = new Pong();
+
+	var goButton = document.querySelector("#js-start");
 
 }
